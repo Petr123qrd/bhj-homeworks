397412b859f0791dce17386fb4888627a560d4a0
Petr123qrd <burduev.p@gmail.com>
2022-04-15 17:03:36 +0300

    first commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,7 +3,7 @@ const links = Array.from(document.querySelectorAll('.menu__link'));
 for (let link of links) {
 	link.onclick = function () {
 		const parent = link.parentElement;
-		if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
+		if (parent.querySelector('ul.menu_sub').className === 'menu menu_sub') {
 			parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
 		} else {
 			parent.querySelector('.menu_sub').className = 'menu menu_sub'
