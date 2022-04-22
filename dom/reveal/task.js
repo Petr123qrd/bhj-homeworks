80795d2672393547b158f2f64b62afdee67952a6
Petr123qrd <burduev.p@gmail.com>
2022-04-22 21:55:49 +0300

    first commit reveal

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,25 +1,24 @@
-const reveal = document.getElementsByClassName('reveal');
+const reveal = document.getElementsByClassName("reveal");
 
 function isVisible(element) {
-    const height = window.innerHeight;
-    const top = element.getBoundingClientRect().top;
-    if (top < 0 && top > height) {
-        return false;
-    } else {
-        return true;
-    }
-    
+  const height = window.innerHeight;
+  const { top } = element.getBoundingClientRect();
+  if (top < 0 && top > height) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
-
-window.addEventListener('scroll', function() {
-    for (let i = 0; i < reveal.length; i++) {
-        if (isVisible(reveal[i]) === true) {
-            reveal[i].classList.add('reveal_active');
-        } else {
-            reveal[i].classList.remove('reveal_active');
-        }
-        
+window.addEventListener("scroll", function () {
+  for (let i = 0; i < reveal.length; i++) {
+    if (isVisible(reveal[i]) === true) {
+      reveal[i].classList.add("reveal_active");
+    } else {
+      reveal[i].classList.remove("reveal_active");
     }
+  }
 });
-    
+
+//const { top } = element.getBoundingClientRect();
+//const top = element.getBoundingClientRect().top;
