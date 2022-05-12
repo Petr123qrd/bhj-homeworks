6d2e81c77df90933cccb04f3772aee63a3d3726f
Вероника Шульгина <verinikashulgina3009@gmail.com>
2022-05-12 15:34:57 +0300

    first commit auth

---
+++ client-state/auth/task.js
@@ -0,0 +1,67 @@
+const signin = document.getElementById('signin');
+const signinForm = document.getElementById('signin__form');
+const signinBtn = document.getElementById('signin__btn');
+const welcome = document.getElementById('welcome');
+const userId = document.getElementById('user_id');
+
+const login = document.querySelectorAll('.control')[0];
+const password = document.querySelectorAll('.control')[1];
+
+authorization();
+
+function verification(e) {
+    if (login.value === '' || password.value === '') {
+        alert('Неверный логин/пароль');
+        signinForm.reset();
+    } else {
+        let formData = new FormData(signinForm);
+        let resData;
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
+        xhr.addEventListener('load', () => {
+            if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+                resData = JSON.parse(xhr.responseText);
+                saveUser(resData);
+                getWelcome(resData);
+            };
+        });
+        xhr.send(formData);
+    };
+    e.preventDefault();
+};
+
+function saveUser(arg) {
+    if (arg.success === true) {
+        localStorage.setItem('user', arg.user_id);
+    };
+};
+
+function authorization() {
+    if (localStorage.user) {
+        getWelcome(localStorage.getItem('user'));
+    } else {
+        signin.classList.add('signin_active');
+    };
+};
+
+function getWelcome(arg) {
+    if (arg.success === true) {
+        signin.classList.remove('signin_active');
+        userId.textContent = arg.user_id;
+        welcome.classList.add('welcome_active');
+    } else {
+        alert('Неверный логин/пароль');
+        signinForm.reset();
+    };
+};
+
+function signOut() {
+    signinForm.reset();
+    welcome.classList.remove('welcome_active');
+    signin.classList.add('signin_active');
+    localStorage.removeItem('user');
+};
+
+signinForm.addEventListener('submit', verification);
+
+signoutBtn.addEventListener('click', signOut);
